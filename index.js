2601d25820038f2118a087d2a1cb0c170184019f
DianaAkinyi <dianaakinyiowiti@gmail.com>
2023-05-22 19:52:58 +0300

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ for (var i = 0; i < h3Tags.length; i++) {
  let img = document.createElement("img");
  img.src = "https://i.pinimg.com/236x/e4/91/b7/e491b7b88147b5d8b6db6b5df39da8af.jpg";
 
- img.style.border = "1px solid orange";
+ img.style.border = "1px solid green";
  img.style.borderRadius = "10px";
 
  document.body.appendChild(img);
@@ -19,8 +19,9 @@ let newFruit = document.createElement("li");
 newFruit.innerHTML = "Banana";
 fruList.appendChild(newFruit);
 
-
 let vegList = document.getElementById("vegList");
 let newVegetable = document.createElement("li");
 newVegetable.innerHTML = "Cherry";
 vegList.appendChild(newVegetable);
+
+
